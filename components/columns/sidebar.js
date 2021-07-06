66bdbffddcbd17e06e903d056565996ba72e9cb2
Ahmet S <ahmetsafa9907@gmail.com>
2021-07-06 20:01:51 +0300

    tweet icon at theme button on flat

--- components/columns/sidebar.js
+++ components/columns/sidebar.js
@@ -5,13 +5,14 @@ import styles from './sidebar.module.css'
 import Navigation from '../navigation/navigation'
 import ThemeButton from '../theme-button'
 import ProfileBox from '../profile-box'
+import {Tweet} from '../icons'
 
 function Sidebar({ flat = false }) {
   return (
     <div className={cn([styles.sidebarCol])}>
       <Navigation flat={flat} />
       <ThemeButton big stretch={!flat} className={styles.tweetBtnMargin}>
-        {flat ? 'a' : 'Tweet'}
+        {flat ? <Tweet/> : 'Tweet'}
       </ThemeButton>
       <ProfileBox flat={flat} size={39} className={styles.avatarMargin} />
     </div>
