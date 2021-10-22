7b7f2e80f48d5d34603b2c5d068c4ca111ae2b94
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-10-22 13:19:15 +0300

    Update index.js

--- components/avatar/index.js
+++ components/avatar/index.js
@@ -5,7 +5,7 @@ import cn from 'classnames'
 import styles from './style.module.css'
 
 function Avatar({
-  src = 'https://raw.githubusercontent.com/AhmetSBulbul/home-sweet-home/main/assets/headIllustration.svg',
+  src = 'https://avatars.githubusercontent.com/u/37499095?s=400&u=8e555dfbacf21003b864d3426c600e0b88e7cb98&v=4',
   alt,
   size = 47
 }) {
